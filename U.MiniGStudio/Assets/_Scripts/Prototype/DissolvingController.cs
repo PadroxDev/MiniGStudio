c6bb243204134da3d3ab6d18ec8cca2132bc62a1
Padrox <antoinevollet.pro@gmail.com>
2024-04-03 16:08:52 +0200

    Upgrade to the proper Unity Editor Version

--- U.MiniGStudio/Assets/_Scripts/Prototype/DissolvingController.cs
+++ U.MiniGStudio/Assets/_Scripts/Prototype/DissolvingController.cs
@@ -4,16 +4,18 @@ using UnityEngine.VFX;
 
 namespace MiniGStudio {
     public class DissolvingController : MonoBehaviour {
-        public SkinnedMeshRenderer SkinnedMesh;
-        public VisualEffect VFXGraph;
-        public float DissolveRate = 0.0125f;
-        public float RefreshRate = 0.025f;
+        [SerializeField] private SkinnedMeshRenderer _skinnedMesh;
+        [SerializeField] private VisualEffect _vfxGraph;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private float _dissolveRate = 0.0125f;
+        [SerializeField] private float _refreshRate = 0.025f;
+        [SerializeField] private float _delay = 0.3f;
 
         private Material[] _skinnedMaterials;
 
         private void Awake() {
-            if (SkinnedMesh is null) return;
-            _skinnedMaterials = SkinnedMesh.materials;
+            if (_skinnedMesh is null) return;
+            _skinnedMaterials = _skinnedMesh.materials;
         }
 
         private void Update() {
@@ -23,19 +25,23 @@ namespace MiniGStudio {
         }
 
         private IEnumerator DissolveCoroutine() {
-            if(VFXGraph is not null) {
-                VFXGraph.Play();
-            }
+            if (_animator is not null)
+                _animator.SetTrigger("Die");
+
+            yield return new WaitForSeconds(_delay);
+
+            if (_vfxGraph is not null)
+                _vfxGraph.Play();
 
             if(_skinnedMaterials.Length > 0) {
                 float counter = 0;
 
                 while (_skinnedMaterials[0].GetFloat("_DissolveAmount") < 1) {
-                    counter += DissolveRate;
+                    counter += _dissolveRate;
                     for (int i = 0; i < _skinnedMaterials.Length; i++) {
                         _skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
                     }
-                    yield return new WaitForSeconds(RefreshRate);
+                    yield return new WaitForSeconds(_refreshRate);
                 }
             }
         }
